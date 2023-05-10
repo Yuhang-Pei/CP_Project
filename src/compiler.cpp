a71857be8f2362e65c7cf2add66f19a3afdb60bc
Pei Yuhang <1522519944@qq.com>
2023-05-10 10:20:58 +0800

    完成LLVM的引入

---
+++ src/compiler.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Pei Yuhang on 2023/5/8.
+//
+
+#include <iostream>
+#include "llvm/IR/Module.h"
+
+using namespace std;
+using namespace llvm;
+
+int main() {
+    std::cout << "This is compiler.cpp" << std::endl;
+}
